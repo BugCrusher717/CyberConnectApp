6e5a21c0792dd9c93bfcbe2a08d5fb5a3387ea7a
WebDev717 <108454005+WebDev717@users.noreply.github.com>
2022-12-29 12:35:40 -0500

    Update
    Ok

--- next.config.js
+++ next.config.js
@@ -1,15 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    webpack: (config, options) => {
-        config.module.rules.push({
-            test: /\.glsl$/,
-            loader: "webpack-glsl-loader",
-        });
-
-        return config;
-    },
+    
 };
 
 module.exports = nextConfig;
